d3a1767df2573655a76b93b650cbdddbe14d52ab
Unknown <kalt.steven@gmail.com>
2017-12-06 12:45:41 -0500

    Added search box that links to the leaflet docs
    The leaflet docs table of contents was scraped using 
    ```
    const as = document.getElementbyId('toc').querySelectorAll('[href]'); 
    Array.from(as).map(a => a.href.replace('leaflet, etc, ...#', '')
    ```

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,22 @@ import '../css/style.css';
 import * as turf from '@turf/helpers'; // for geojson creation
 import {parse} from 'query-string'
 window.turf = turf;
+import leafletRefs from '../json/leaflet-refs.json';
+function openDocumentation(ref){
+  debugger;
+  window.open('http://leafletjs.com/reference-1.2.0.html#' + ref, '_blank');
+}
+let datalist = document.getElementById('leaflet-refs');
+const options = leafletRefs.map(r => `<option value="${r}">${r}</option>`).join();
+datalist.innerHTML = options;
+document.querySelector('input').addEventListener('select', e =>{
+    openDocumentation(e.target.value)
+});
 // decipher the intended center
 const query = parse(window.location.search);
 const center = query.center
   || query.lat != undefined && query.lng != undefined && [query.lng, query.lat]
-  || [42.36,-71.0589];
+  || [42.36, -71.0589];
 const zoom = 13;
 window.map = L.map('map', {center, zoom});
 

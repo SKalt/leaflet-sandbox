3684ced89d56b5e17ae7fd5a69b014054a0572a9
Unknown <kalt.steven@gmail.com>
2017-12-06 12:01:29 -0500

    Centered map on coordinates from querystring

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,19 @@
 import '../css/style.css';
-// initialize the map on the "map" div with a given center and zoom
-window.map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
-});
 import * as turf from '@turf/helpers'; // for geojson creation
+import {parse} from 'query-string'
 window.turf = turf;
+// decipher the intended center
+const query = parse(window.location.search);
+const center = query.center
+  || query.lat != undefined && query.lng != undefined && [query.lng, query.lat]
+  || [42.36,-71.0589];
+const zoom = 13;
+window.map = L.map('map', {center, zoom});
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-window.marker = L.marker([51.5, -0.09]).addTo(map)
+window.marker = L.marker(center).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();

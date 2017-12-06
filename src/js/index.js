2ecf5431f48b86d4a7541c822e0dcc38cba8ec3d
Unknown <kalt.steven@gmail.com>
2017-12-06 11:42:00 -0500

    Added minimal turf helpers
    For geojson manipulation

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,8 @@ window.map = L.map('map', {
     center: [51.505, -0.09],
     zoom: 13
 });
+import * as turf from '@turf/helpers'; // for geojson creation
+window.turf = turf;
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
